a3dc316f047d7f43be943828fb4c925a5ae472b8
Lance Lu <lance.cl.lu@gmail.com>
2024-01-25 15:01:25 +0800

    add profile python

---
+++ components/crud-web-apps/common/backend/kubeflow/kubeflow/crud_backend/api/profile.py
@@ -0,0 +1,22 @@
+from kubernetes.config.config_exception import ConfigException
+from . import custom_api
+
+def get_all_profiles():
+  # get cluster custom object profiles
+  profile_list = custom_api.list_cluster_custom_object(
+    group="kubeflow.org",
+    version="v1beta1",
+    plural="profiles",
+  )
+  return profile_list
+
+
+def get_profile(profile_name):
+  # get cluster custom object profile
+  profile = custom_api.get_cluster_custom_object(
+    group="kubeflow.org",
+    version="v1beta1",
+    plural="profiles",
+    name=profile_name
+  )
+  return profile
