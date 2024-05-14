0fb670e977fc2fb5b4dcb50dd2ff9ef3eb5ba1aa
je5577iee <127774549+je5577iee@users.noreply.github.com>
2024-05-14 11:16:11 +0800

    Update app-routing.module.ts

--- components/crud-web-apps/jupyter/frontend/src/app/app-routing.module.ts
+++ components/crud-web-apps/jupyter/frontend/src/app/app-routing.module.ts
@@ -3,10 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { IndexComponent } from './pages/index/index.component';
 import { FormComponent } from './pages/form/form.component';
+import { IndexComponent2 } from './pages/index2/index2.component';
 
 const routes: Routes = [
   { path: '', component: IndexComponent },
   { path: 'new', component: FormComponent },
+  { path: 'notebook/auto-start/:namespace/:notebook_name/view', component: IndexComponent2},
+  { path: 'notebook/auto-start/:namespace/:notebook_name', component: IndexComponent2}
 ];
 
 @NgModule({
